baf8e23bb18a2606fdda47a4b9d0205ac882f608
yaa-zalmaty <jclarksummit@gmail.com>
2022-12-21 17:46:35 -0500

    Added grid line toggle feature

--- scripts.js
+++ scripts.js
@@ -1,20 +1,68 @@
+// Initialize + populate grid on page load
+let singleCell;
+let gridContainer = document.getElementById('grid-container');
+populateGrid(24);
 
-addCells(24);
+// Slider functionality
+
+let sizeSlider = document.getElementById("size-slider");
+sizeSlider.addEventListener('change', changeGridSize);
+
+function changeGridSize() {
+    let gridSize = sizeSlider.valueAsNumber;
+    clearGrid();
+    populateGrid(gridSize);
+    updateGridSizeText(gridSize);
+    allGridItems = document.querySelectorAll(".single-cell")
+    // Check for and generate outline MAYBE CLEAN THIS UP?
+    allGridItems.forEach(function (gridItem) {
+        if (outlineVisible) {
+            gridItem.style.outline = "1px solid black";
+        } else {
+            gridItem.style.outline = "";
+        }
+    });
+}
+
+// Toggle grid lines
+let outlineVisible = false;
+let allGridItems = document.querySelectorAll(".single-cell");
+let gridLinesButton = document.getElementById("toggle-grid-lines");
+gridLinesButton.addEventListener('click', toggleGridLines)
+toggleGridLines();
+
+function toggleGridLines() {
+    outlineVisible = !outlineVisible;
+    allGridItems.forEach(function (gridItem) {
+        if (outlineVisible) {
+            gridItem.style.outline = "1px solid black";
+        } else {
+            gridItem.style.outline = "";
+        }
+    });
+}
 
 // Functions
 
-function addCells(num){
-    let singleCell;
-    let gridContainer;
+function populateGrid(num) {
     let dimensions = 'calc(100%/ ' + num + ')';
-    console.log(dimensions)
-    for (let i = 0; i < num * num; i++){
+    for (let i = 0; i < num * num; i++) {
         singleCell = document.createElement('div');
         singleCell.classList.add('single-cell');
         singleCell.style.width = dimensions;
         singleCell.style.height = dimensions;
 
-        gridContainer = document.getElementById('grid-container');
         gridContainer.appendChild(singleCell)
     }
+}
+
+function clearGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+function updateGridSizeText(gridSize){
+    let gridSizeText = document.getElementById("size-indicator-text");
+    gridSizeText.textContent = `Grid Size: ${gridSize} x ${gridSize}`
 }
